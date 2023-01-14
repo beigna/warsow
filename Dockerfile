12380cce3c8995c06038089fea332edbd0b2f4d9
Mauro <mauro@sdf.org>
2023-01-14 17:25:05 -0300

    Adding dedicated_autoexec + updated dockerfile and docker-compose + removed settings.env

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,8 @@ ADD https://warsow.net/$WARSOW_RELEASE.tar.gz /opt
 RUN cd /opt && tar xzvf $WARSOW_RELEASE.tar.gz
 
 COPY run.sh $HOME
+COPY cfg/dedicated_autoexec.cfg $HOME/basewsw/dedicated_autoexec.cfg
+
 RUN groupadd -g $GID $USER && \
     useradd -u $UID -g $USER -s /bin/bash $USER && \
     chmod +x $HOME/run.sh && \
@@ -18,4 +20,7 @@ RUN groupadd -g $GID $USER && \
 
 USER $USER
 WORKDIR $HOME
+EXPOSE 44400
+EXPOSE 44444
+
 CMD ["/bin/bash", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-params=""
-
-if [ -n "$SV_HOSTNAME" ]; then
-    params+=" +sv_hostname \"${SV_HOSTNAME}\""
-fi
-
-if [ -n "$SV_AUTOUPDATE" ]; then
-    params+=" +sv_autoupdate $SV_AUTOUPDATE"
-fi
-
-if [ -n "$G_MAPROTATION" ]; then
-    params+=" +g_maprotation $G_MAPROTATION"
-fi
-
-if [ -n "$RCON_PASSWORD" ]; then
-    params+=" +rcon_password $RCON_PASSWORD"
-fi
-
-exec ./wsw_server $params
+./wsw_server +set fs_usehomedir 0 +set fs_basepath $HOME +set dedicated 1
